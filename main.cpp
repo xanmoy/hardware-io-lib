eb433acfbbc5e699dab29f706762f43e0ca09926
xanmoy <xanmoy@outlook.com>
2023-12-29 00:09:13 +0530

    updated

--- main.cpp
+++ main.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+// g++ main.cpp - o main - lcurl
+
+size_t
+WriteCallback(void *contents, size_t size, size_t nmemb, std::string *output)
+{
+    size_t total_size = size * nmemb;
+    output->append(static_cast<char *>(contents), total_size);
+    return total_size;
+}
+
+std::string MakeHttpRequest(const std::string &url)
+{
+    CURL *curl;
+    CURLcode res;
+    std::string response;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
+        {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+
+    return response;
+}
 
 int main()
 {
-    std::cout << "Hello World!";
+    std::string url = "https://www.google.com";
+    std::string response = MakeHttpRequest(url);
+
+    std::cout << "Response:\n"
+              << response << std::endl;
+
     return 0;
 }
